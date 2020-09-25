43bb52dc4c0cc486867ac706533e5ba8c88c924d
Admin <pandyatirth08@gmail.com>
2020-09-25 18:35:10 +0530

    Updates

---
+++ precache-manifest.1e7cb63cc5adbe3efcf089bf3051a0a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d452cdd7bb3a450e2f7d128a324defa2",
+    "url": "/Truth-Or-Dare/index.html"
+  },
+  {
+    "revision": "6f97c54c2b45acb3a294",
+    "url": "/Truth-Or-Dare/static/css/main.43ca2c5f.chunk.css"
+  },
+  {
+    "revision": "12f8f360e57227cfe91a",
+    "url": "/Truth-Or-Dare/static/js/2.3a1a4e2b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Truth-Or-Dare/static/js/2.3a1a4e2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f97c54c2b45acb3a294",
+    "url": "/Truth-Or-Dare/static/js/main.a00e0dfe.chunk.js"
+  },
+  {
+    "revision": "172781fc1b27a71ab56a",
+    "url": "/Truth-Or-Dare/static/js/runtime-main.cfdf2c6b.js"
+  }
+]);
